cccfd4c86112a7d5a6bcec725c07414ee4c99e88
Lucas Casals <lrcasals01@bvsd.org>
2019-08-21 20:15:05 -0600

    added attributes, and constructors

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,4 @@
+
 public class BankAccount
 {
     private double balance;
@@ -9,7 +10,11 @@ public class BankAccount
     public BankAccount(double balance, String accountHolder, int accountNumber,
     String password)
     {
-
+        this.balance = balance;
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.password = password;
+        this.loggedIn = false;
     }
     
     //mutator - allow you to change an instance variable (attribute)
@@ -24,6 +29,20 @@ public class BankAccount
     //accessor - gives access to the balance instance variable (attribute)
     public double getBalance()
     {
-        return 0;
-    }  
+        if(loggedIn)
+        {
+            return balance;
+        }
+      else
+        {
+            throw new IllegalStateException("YOU GAY");
+        }
+    }
+
+    //log user out
+    
+    public void logOut()
+    {
+        loggedIn = false;
+    }
 }
