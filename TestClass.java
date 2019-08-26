e2d8818ab5f555c7559d9844b2cde87cf2ba8c6e
Lucas Casals <lrcasals01@bvsd.org>
2019-08-26 12:49:07 -0600

    Added access to BankAccount

--- TestClass.java
+++ TestClass.java
@@ -8,9 +8,15 @@ public class TestClass
 {
     public static void main(String[] args)
     {
-        BankAccount a1 = new BankAccount(500, "gesell", 123, "pass");
-        a1.balance = 1000000;
-        a1.password = "newpass";
+        BankAccount a1 = new BankAccount(500, "Lucas", 123, "pass");
+        a1.logIn("Lucas", "pass");
+        System.out.println(a1);
         
+        a1.Deposit(10000);
+        System.out.println(a1);
+        
+        a1.Withdraw(500);
+        System.out.println(a1);
+
     }
 }
